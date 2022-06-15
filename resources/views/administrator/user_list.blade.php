f4df25b5e236c618d9ff618cbc73770e3b11ba9a
AMNV <87751322+asmajati15@users.noreply.github.com>
2022-06-16 05:42:53 +0700

    Update user_list.blade.php

--- resources/views/administrator/user_list.blade.php
+++ resources/views/administrator/user_list.blade.php
@@ -24,7 +24,7 @@
   </div>
   <div class="card mb-4">
     <div class="card-header pb-0">
-      <h6>Daftar User</h6>
+      <h6>Daftar Admin</h6>
     </div>
     <div class="card-body px-0 pt-0 pb-2">
       <div class="table-responsive p-0">
